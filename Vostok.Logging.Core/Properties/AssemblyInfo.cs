9fdfbece6018f098076048ccee4d27ecdf01cf66
Krait <krait@skbkontur.ru>
2018-07-19 17:03:54 +0500

    Fixed build for dependent tests projects.

--- Vostok.Logging.Core/Properties/AssemblyInfo.cs
+++ Vostok.Logging.Core/Properties/AssemblyInfo.cs
@@ -3,4 +3,6 @@
 [assembly: InternalsVisibleTo("Vostok.Logging.ConsoleLog")]
 [assembly: InternalsVisibleTo("Vostok.Logging.FileLog")]
 [assembly: InternalsVisibleTo("Vostok.Logging.Core.Tests")]
+[assembly: InternalsVisibleTo("Vostok.Logging.ConsoleLog.Tests")]
+[assembly: InternalsVisibleTo("Vostok.Logging.FileLog.Tests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
