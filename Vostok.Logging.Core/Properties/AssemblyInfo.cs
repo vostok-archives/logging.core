0237fc5f2814d92be667017adf85820b087dc46c
Krait <krait@skbkontur.ru>
2018-07-31 16:03:19 +0500

    Removed InternalsVisibleTo.

--- Vostok.Logging.Core/Properties/AssemblyInfo.cs
+++ Vostok.Logging.Core/Properties/AssemblyInfo.cs
@@ -1,8 +1,4 @@
 ﻿using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("Vostok.Logging.ConsoleLog")]
-[assembly: InternalsVisibleTo("Vostok.Logging.FileLog")]
 [assembly: InternalsVisibleTo("Vostok.Logging.Core.Tests")]
-[assembly: InternalsVisibleTo("Vostok.Logging.ConsoleLog.Tests")]
-[assembly: InternalsVisibleTo("Vostok.Logging.FileLog.Tests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
